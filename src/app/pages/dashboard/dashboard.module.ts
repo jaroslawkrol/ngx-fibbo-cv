6b5019d99451e39ea396dbb6fa7ca90ae05b0355
Jarosław Król <jaroslaw.krol@execon.pl>
2017-07-10 00:14:38 +0200

    My own signature and changes in eefil styles.

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import {SharedModule} from "../../shared/shared.module";
+import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    PerfectScrollbarModule.forChild(),
   ],
   declarations: [DashboardComponent],
   exports: []
